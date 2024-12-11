cc7cdfa94b38a8c6ec2d20e57d7ce93187c72e0f
Donovan Liu <144486880+donovanliu1@users.noreply.github.com>
2024-12-11 14:50:21 -0500

    need to finish part 2

--- Day11.java
+++ Day11.java
@@ -6,19 +6,50 @@ import java.util.Scanner;
 public class Day11{
     public static void main(String[] args){
         ArrayList<String> fileData = getFileData("input.txt");
-
         System.out.println("Part 1: " + doPartOne(fileData));
         System.out.println("Part 2: " + doPartTwo(fileData));
     }
 
     public static int doPartOne(ArrayList<String> fileData){
-        int count = 0;
-        return count;
+        String[] tempStones = fileData.get(0).split(" ");
+        ArrayList<String> stones = new ArrayList<>();
+        for (String stone : tempStones) stones.add(stone);
+        int blinks = 25;
+        for (int blink = 0; blink < blinks; blink++){
+            for (int stone = 0; stone < stones.size(); stone++){
+                String currentStone = stones.get(stone);
+                if (Long.parseLong(currentStone) == 0) stones.set(stone, String.valueOf(1));
+                else if (currentStone.length() % 2 == 0){
+                    long stoneRight = Long.parseLong(currentStone.substring(currentStone.length()/2));
+                    stones.set(stone, String.valueOf(stoneRight));
+                    stones.add(stone, currentStone.substring(0, currentStone.length()/2));
+                    stone++;
+                }
+                else stones.set(stone, String.valueOf(Long.parseLong(currentStone) * 2024));
+            }
+        }
+        return stones.size();
     }
     
     public static int doPartTwo(ArrayList<String> fileData){
-        int count = 0;
-        return count;
+        String[] tempStones = fileData.get(0).split(" ");
+        ArrayList<String> stones = new ArrayList<>();
+        for (String stone : tempStones) stones.add(stone);
+        int blinks = 75;
+        for (int blink = 0; blink < blinks; blink++){
+            for (int stone = 0; stone < stones.size(); stone++){
+                String currentStone = stones.get(stone);
+                if (Long.parseLong(currentStone) == 0) stones.set(stone, String.valueOf(1));
+                else if (currentStone.length() % 2 == 0){
+                    long stoneRight = Long.parseLong(currentStone.substring(currentStone.length()/2));
+                    stones.set(stone, String.valueOf(stoneRight));
+                    stones.add(stone, currentStone.substring(0, currentStone.length()/2));
+                    stone++;
+                }
+                else stones.set(stone, String.valueOf(Long.parseLong(currentStone) * 2024));
+            }
+        }
+        return stones.size();
     }
 
     public static ArrayList<String> getFileData(String fileName) {

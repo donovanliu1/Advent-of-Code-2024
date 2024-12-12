59f5d1559cf49f4c034118e94a784c1e155a6f2d
Donovan Liu <144486880+donovanliu1@users.noreply.github.com>
2024-12-12 14:51:26 -0500

    got combinations

--- Day07.java
+++ Day07.java
@@ -1,10 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.util.ArrayList;
 
 
@@ -20,18 +16,17 @@ public class Day07 {
 
         ArrayList<String> testValues = new ArrayList<>();
         ArrayList<String[]> calibrationNums = new ArrayList<>();
-        for (String data :fileData) {
-            String[] split = data.split(":");
-            testValues.add(split[0]);
-            calibrationNums.add(split[1].substring(1).split(" "));
-        }
-
+//        for (String data :fileData) {
+//            String[] split = data.split(":");
+//            testValues.add(split[0]);
+//            calibrationNums.add(split[1].substring(1).split(" "));
+//        }
+        checkCalibration("190", new String[]{"19", "10", "123", "123"});
         return sum;
     }
 
     // combinations of operations
     // for loop range: list size * 2
-
 
     public static boolean checkCalibration(String testValue, String[] calibrationNums){
         int tVal = Integer.parseInt(testValue);
@@ -39,17 +34,58 @@ public class Day07 {
         for (int i = 0; i < calibrationNums.length; i++){
             cNums[i] = Integer.parseInt(calibrationNums[i]);
         }
-
-
-
+        ArrayList<String[]> binaries = new ArrayList<>();
+        String[] binary = new String[]{"+","*"};
+        int size = calibrationNums.length - 1;
+        for (int i = 0; i < size; i++){
+            binaries.add(binary);
+        }
+        ArrayList<ArrayList<String>> combinations = getCombinations(binaries);
+        System.out.println(combinations);
         return false;
     }
 
-    public static boolean checkNums(int testValue, int[] calilbrationNums, int position, int currentNum, ArrayList<Integer> calibrationResults){
-        if (position < 0) return testValue == currentNum;
-
-        return false;
+    public static ArrayList<ArrayList<String>> getCombinations(ArrayList<String[]> binaries){
+        ArrayList<ArrayList<String>> combinations = new ArrayList<>();
+        getCombinationsHelper(binaries, 0, new ArrayList<>(), combinations);
+        return combinations;
     }
+
+    public static void getCombinationsHelper(ArrayList<String[]> binaries, int index, ArrayList<String> current, ArrayList<ArrayList<String>> result){
+        if (index == binaries.size()){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        String[] currentBinary = binaries.get(index);
+//        ArrayList<String> currentBinary = binaries.get(index);
+        for (String binary : currentBinary){
+            current.add(binary);
+            getCombinationsHelper(binaries, index+1, current, result);
+            current.removeLast();
+        }
+    }
+
+//    public static boolean checkNums(int testValue, int[] calilbrationNums, int position, int currentNum, ArrayList<Integer> calibrationResults){
+//        if (position < 0) return testValue == currentNum;
+//
+//        return false;
+//    }
+//
+////    public static String[] getCombinations (int[] calibrationNums){
+////        int combinationSize = calibrationNums.length - 1;
+////        int arraySize = (int) Math.pow(2, combinationSize);
+////        String[] combinations = new String[combinationSize];
+////        for (int i = 0; i < combinations.length; i++){
+////            String combination = "";
+////            for (int j = 0; j < )
+////        }
+////    }
+//
+//    public static String getCombinations(ArrayList<String> combinations, int[] calibrationNums, int position, boolean binary){
+//        if (position < calibrationNums.length){
+//            if (binary) return "+" + getCombination(combinations, calibrationNums, position + 1, )
+//        }
+//    }
 
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
